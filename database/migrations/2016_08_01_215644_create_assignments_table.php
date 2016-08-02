37e2e53f7dd76d84da13f795dfe00b6c7256b7f9
Luis Herrera <lherrera@gignosoft.cl>
2016-08-01 21:05:10 -0400

    commit inicial

---
+++ database/migrations/2016_08_01_215644_create_assignments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('assignments', function (Blueprint $table){
+
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('description');
+
+            $table->string('user_control');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('assignments');
+    }
+}
