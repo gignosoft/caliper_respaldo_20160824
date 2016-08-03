a71760fbc0151d89d603b2e7e0979f54d772e4ba
Luis Herrera <lherrera@gignosoft.cl>
2016-08-03 17:06:48 -0400

    se modifica tabla users

--- database/migrations/2016_08_01_153328_create_users_table.php
+++ database/migrations/2016_08_01_153328_create_users_table.php
@@ -14,7 +14,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('identifier')->unique();
+            $table->string('fist_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->integer('city_id')->unsigned();
